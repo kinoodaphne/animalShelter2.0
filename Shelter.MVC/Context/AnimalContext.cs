3ce47e6743c53e8ea968733716ba8f339632a913
Vince van alphen <vincevanalphen@yahoo.com>
2019-12-06 00:19:10 +0100

    finished AnimalContext

--- Shelter.MVC/Context/AnimalContext.cs
+++ Shelter.MVC/Context/AnimalContext.cs
@@ -1,8 +1,17 @@
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Shelter.Shared;
 
 namespace Shelter.MVC.Context {
     public class AnimalContext : DbContext{
+        public AnimalContext(DbContextOptions<AnimalContext> options) : base(options){
+
+        }
         public DbSet<Animal> Animals { get; set; }
+        public DbSet<Shelter> Shelters { get; set; }
+        public DbSet<Employee> Employees { get; set; }
     }
 }
