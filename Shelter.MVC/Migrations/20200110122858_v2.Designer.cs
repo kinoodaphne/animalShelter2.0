5abb4bd3744418dbf1a102553baabaa4e13b1944
Vince van alphen <vincevanalphen@yahoo.com>
2020-01-10 19:45:17 +0100

    new migrations of DB

--- Shelter.MVC/Migrations/20200110122858_v2.Designer.cs
+++ Shelter.MVC/Migrations/20200110122858_v2.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shelter.MVC.Context;
 
 namespace Shelter.MVC.Migrations
 {
     [DbContext(typeof(ShelterContext))]
-    partial class ShelterContextModelSnapshot : ModelSnapshot
+    [Migration("20200110122858_v2")]
+    partial class v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
